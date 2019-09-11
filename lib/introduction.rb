47886cab626f4b813c9c4601a6bf86dbe68ae3e5
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-09-11 21:13:09 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,11 +2,8 @@ def introduction(name)
   "Hi, my name is #{name}."
 end 
 
-def introduction_with_language(name)
+def introduction_with_language(name, language)
+  "Hi, my name is #{name} and I am learning to program in #{language}."
+end
   
 
-describe "#introduction_with_language" do 
-  it "takes in two arguments, a name and a language, and puts out a phrase using those arguments." do 
-    expect{introduction_with_language("Dan", "Ember.js")}.to output("Hi, my name is Dan and I am learning to program in Ember.js.\n").to_stdout
-  end
-end
